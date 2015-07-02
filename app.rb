b267372b0b36caa7e87549993b2010119ab0e0c9
Jason Benn <jasoncbenn@gmail.com>
2015-07-02 12:34:05 -0700

    HTTPS.

--- app.rb
+++ app.rb
@@ -11,18 +11,15 @@ __END__
   <head>
     <title>Benn Family Interviews</title>
     <meta charset="utf-8" />
-    <style>
-      
-    </style>
   </head>
   <body>
-    <video src="http://family-interviews.s3.amazonaws.com/Cathy%2052%20part%201%20of%202-HD.mp4" controls></video>
-    <video src="http://family-interviews.s3.amazonaws.com/Cathy%2052%20part%202%20of%202-HD.mp4" controls></video>
-    <video src="http://family-interviews.s3.amazonaws.com/Jason%2025-HD.mp4" controls></video>
-    <video src="http://family-interviews.s3.amazonaws.com/Melissa%2023-HD.mp4" controls></video>
-    <video src="http://family-interviews.s3.amazonaws.com/Randy%2053%20part%201%20of%204-HD.mp4" controls></video>
-    <video src="http://family-interviews.s3.amazonaws.com/Randy%2053%20part%202%20of%204-HD.mp4" controls></video>
-    <video src="http://family-interviews.s3.amazonaws.com/Randy%2053%20part%203%20of%204-HD.mp4" controls></video>
-    <video src="http://family-interviews.s3.amazonaws.com/Randy%2053%20part%204%20of%204-HD.mp4" controls></video>
+    <video src="https://family-interviews.s3.amazonaws.com/Cathy%2052%20part%201%20of%202-HD.mp4" controls></video>
+    <video src="https://family-interviews.s3.amazonaws.com/Cathy%2052%20part%202%20of%202-HD.mp4" controls></video>
+    <video src="https://family-interviews.s3.amazonaws.com/Jason%2025-HD.mp4" controls></video>
+    <video src="https://family-interviews.s3.amazonaws.com/Melissa%2023-HD.mp4" controls></video>
+    <video src="https://family-interviews.s3.amazonaws.com/Randy%2053%20part%201%20of%204-HD.mp4" controls></video>
+    <video src="https://family-interviews.s3.amazonaws.com/Randy%2053%20part%202%20of%204-HD.mp4" controls></video>
+    <video src="https://family-interviews.s3.amazonaws.com/Randy%2053%20part%203%20of%204-HD.mp4" controls></video>
+    <video src="https://family-interviews.s3.amazonaws.com/Randy%2053%20part%204%20of%204-HD.mp4" controls></video>
   </body>
 </html>
