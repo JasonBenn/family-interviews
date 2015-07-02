5bca20de6ad8d2534303612fddc1782cdf4625c5
Jason Benn <jasoncbenn@gmail.com>
2015-07-02 12:29:06 -0700

    Simplest Heroku/Sinatra app possible.

---
+++ app.rb
@@ -0,0 +1,28 @@
+require 'sinatra'
+
+get '/' do
+  erb :home
+end
+
+__END__
+
+@@ home
+<html>
+  <head>
+    <title>Benn Family Interviews</title>
+    <meta charset="utf-8" />
+    <style>
+      
+    </style>
+  </head>
+  <body>
+    <video src="http://family-interviews.s3.amazonaws.com/Cathy%2052%20part%201%20of%202-HD.mp4" controls></video>
+    <video src="http://family-interviews.s3.amazonaws.com/Cathy%2052%20part%202%20of%202-HD.mp4" controls></video>
+    <video src="http://family-interviews.s3.amazonaws.com/Jason%2025-HD.mp4" controls></video>
+    <video src="http://family-interviews.s3.amazonaws.com/Melissa%2023-HD.mp4" controls></video>
+    <video src="http://family-interviews.s3.amazonaws.com/Randy%2053%20part%201%20of%204-HD.mp4" controls></video>
+    <video src="http://family-interviews.s3.amazonaws.com/Randy%2053%20part%202%20of%204-HD.mp4" controls></video>
+    <video src="http://family-interviews.s3.amazonaws.com/Randy%2053%20part%203%20of%204-HD.mp4" controls></video>
+    <video src="http://family-interviews.s3.amazonaws.com/Randy%2053%20part%204%20of%204-HD.mp4" controls></video>
+  </body>
+</html>
