13fb696f45e15b6195bc2a31242f42d275ea2f39
Jason Benn <jasoncbenn@gmail.com>
2015-07-02 12:47:11 -0700

    Only load metadata - S3 can only sustain so many connections at a time when it's trying to send you the entire video files. Add labels and simple styles.

--- app.rb
+++ app.rb
@@ -11,15 +11,50 @@ __END__
   <head>
     <title>Benn Family Interviews</title>
     <meta charset="utf-8" />
+    <style>
+    body {
+      display: flex;
+      flex-flow: row wrap;
+    }
+    .group {
+      margin: 1em;
+      color: #222;
+      font-family: Helvetica;
+    }
+    </style>
   </head>
   <body>
-    <video src="https://family-interviews.s3.amazonaws.com/Cathy%2052%20part%201%20of%202-HD.mp4" controls></video>
-    <video src="https://family-interviews.s3.amazonaws.com/Cathy%2052%20part%202%20of%202-HD.mp4" controls></video>
-    <video src="https://family-interviews.s3.amazonaws.com/Jason%2025-HD.mp4" controls></video>
-    <video src="https://family-interviews.s3.amazonaws.com/Melissa%2023-HD.mp4" controls></video>
-    <video src="https://family-interviews.s3.amazonaws.com/Randy%2053%20part%201%20of%204-HD.mp4" controls></video>
-    <video src="https://family-interviews.s3.amazonaws.com/Randy%2053%20part%202%20of%204-HD.mp4" controls></video>
-    <video src="https://family-interviews.s3.amazonaws.com/Randy%2053%20part%203%20of%204-HD.mp4" controls></video>
-    <video src="https://family-interviews.s3.amazonaws.com/Randy%2053%20part%204%20of%204-HD.mp4" controls></video>
+    <div class="group">
+      <h1>Cathy, 52 (part 1 of 2)</h1>
+      <video controls preload="metadata" src="https://family-interviews.s3.amazonaws.com/Cathy%2052%20part%201%20of%202-HD.mp4"></video>
+    </div>
+    <div class="group">
+      <h1>Cathy, 52 (part 2 of 2)</h1>
+      <video controls preload="metadata" src="https://family-interviews.s3.amazonaws.com/Cathy%2052%20part%202%20of%202-HD.mp4"></video>
+    </div>
+    <div class="group">
+      <h1>Jason, 25</h1>
+      <video controls preload="metadata" src="https://family-interviews.s3.amazonaws.com/Jason%2025-HD.mp4"></video>
+    </div>
+    <div class="group">
+      <h1>Melissa, 23</h1>
+      <video controls preload="metadata" src="https://family-interviews.s3.amazonaws.com/Melissa%2023-HD.mp4"></video>
+    </div>
+    <div class="group">
+      <h1>Randy, 53 (part 1 of 4)</h1>
+      <video controls preload="metadata" src="https://family-interviews.s3.amazonaws.com/Randy%2053%20part%201%20of%204-HD.mp4"></video>
+    </div>
+    <div class="group">
+      <h1>Randy, 53 (part 2 of 4)</h1>
+      <video controls preload="metadata" src="https://family-interviews.s3.amazonaws.com/Randy%2053%20part%202%20of%204-HD.mp4"></video>
+    </div>
+    <div class="group">
+      <h1>Randy, 53 (part 3 of 4)</h1>
+      <video controls preload="metadata" src="https://family-interviews.s3.amazonaws.com/Randy%2053%20part%203%20of%204-HD.mp4"></video>
+    </div>
+    <div class="group">
+      <h1>Randy, 53 (part 4 of 4)</h1>
+      <video controls preload="metadata" src="https://family-interviews.s3.amazonaws.com/Randy%2053%20part%204%20of%204-HD.mp4"></video>
+    </div>
   </body>
 </html>
